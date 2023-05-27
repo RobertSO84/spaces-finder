530069ce42ca25315cf4ade1d33c58466d4e8739
RobertSO84 <robert.oropeza@gmail.com>
2023-05-27 16:45:57 -0600

    Finished getSpaces getItem

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,10 +1,18 @@
 import { handler } from "../src/services/spaces/handler";
 
+process.env.AWS_REGION = "us-east-2";
+process.env.TABLE_NAME = "SpaceStack-02dc23b96807";
 
 
-handler({
+handler(
+  {
     httpMethod: "GET",
+    queryStringParameters: {
+      id: "26a38b79-5587-4a2c-8484-6e2f551442d0",
+    },
     // body: JSON.stringify({
     //     location: "London"
     // })
-} as any, {} as any);
+  } as any,
+  {} as any
+);
